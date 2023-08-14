7789f2199a1245fd531c23cc08efc13371e75f5b
KomalSoni26 <81246076+KomalSoni26@users.noreply.github.com>
2023-08-14 19:16:22 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+console.log(" I M learning javascript syntax");
+
+var first_name="Komal";
+console.log(first_name);
+
+var last_name ="Soni";
+console.log(last_name);
+
+var full_name= first_name.concat(last_name);
+console.log(full_name);
+
+var length= full_name.length;
+console.log(length);
+ 
